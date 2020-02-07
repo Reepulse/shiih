3f0e5330ea42bc8b236cd90355bded1d93252ed6
Reepulse <reepulse@gmail.com>
2020-02-07 12:48:25 +0530

    removed theme files

--- js/testimonial.js
+++
@@ -1,8 +0,0 @@
-(function ($) {
-    "use strict";
-	//testimonial carousel setting
-    $(".testimonial:has(>li:eq(1)),.norotatingtweets:has(>div:eq(1))").list_ticker({
-        speed: 5000,
-        effect: 'fade'
-    });
-})(jQuery);
